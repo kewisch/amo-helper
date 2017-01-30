535aeb4515dfb4abc06e8b6aa6af32d2dcf6f754
Philipp Kewisch <mozilla@kewis.ch>
2017-01-30 23:00:53 +0100

    Allow selecting multiple lines in the file browser

--- webextension/content/copylink/filebrowser.js
+++ webextension/content/copylink/filebrowser.js
@@ -3,8 +3,14 @@ function onclickln(event) {
   let filename = $(".file.selected").attr("data-short");
   let line = linelink.attr("data-linenumber");
 
+  let text = filename + " line " + line;
+  if (event.metaKey) {
+    text = last_copy_data + ", " + text;
+  }
+  last_copy_data = text;
+
   let copybox = $("<textarea/>");
-  copybox.text(filename + " line " + line);
+  copybox.text(text);
   $("body").append(copybox);
   copybox.select();
   document.execCommand("copy");
@@ -13,4 +19,5 @@ function onclickln(event) {
   event.stopPropagation();
 }
 
+var last_copy_data = "";
 $("#file-viewer-inner").on("click", ".td-line-number a", onclickln);
--- webextension/content/copylink/review.js
+++ webextension/content/copylink/review.js
@@ -1,14 +1,32 @@
 function condenseLines(value) {
   const regex = /([A-Za-z0-9_\.\/]+) line ([0-9, ]+), ([A-Za-z0-9_\.\/]+) line (\d+)/g;
-  return value.replace(regex, (match, file1, line1, file2, line2) => {
-    if (file1 != file2) {
-      return match;
-    }
-    let lines = new Set(line1.split(/,\s*/));
-    lines.add(line2.trim());
+  const regex2 = /([A-Za-z0-9_\.\/]+) line ([0-9, ]+)/g;
+  let replaced;
 
-    return `${file1} line ${[...lines].join(", ")}`;
+  do {
+    replaced = false;
+    value = value.replace(regex, (match, file1, line1, file2, line2, ...extra) => {
+      console.log(` replaced ${file1} ${line1} / ${file2} ${line2} / ${extra}`);
+      if (file2 && file1 != file2) {
+        return match;
+      }
+      let lines = new Set(line1.split(/,\s*/));
+      if (file2) {
+        lines.add(line2.trim());
+      }
+      replaced = true;
+
+      return `${file1} line ${[...lines].join(", ")}`;
+    });
+  } while (replaced);
+
+  // no idea how to get this into the first regex
+  value = value.replace(regex2, (match, file, lines) => {
+    let lineset = new Set(lines.split(/,\s*/));
+    return `${file} line ${[...lineset].join(", ")}`;
   });
+
+  return value;
 }
 
 let comments = document.getElementById("id_comments");
