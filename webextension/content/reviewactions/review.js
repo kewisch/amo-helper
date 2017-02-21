23d316ee52a74c091a4f03315475c7f7b2578a3c
Philipp Kewisch <mozilla@kewis.ch>
2017-02-21 15:40:28 +0100

    Add a copy slug link to the review actions

---
+++ webextension/content/reviewactions/review.js
@@ -0,0 +1,29 @@
+function createAction(label, funcOrDest, className) {
+  let actions = document.getElementById("actions-addon");
+  let dest = typeof funcOrDest == "string" ? funcOrDest : "#";
+  let func = typeof funcOrDest == "string" ? null : funcOrDest;
+
+  let item = actions.appendChild(document.createElement("li"));
+  let link = item.appendChild(document.createElement("a"));
+
+  link.setAttribute("href", dest);
+  link.textContent = label;
+  link.className = "amoqueue-action-link " + className;
+
+  if (func) {
+    link.addEventListener("click", func);
+  }
+}
+
+createAction("Copy Slug", (event) => {
+  let textarea = document.createElement("textarea");
+  textarea.value = document.location.href.match(/\/([^\/]+)$/)[1];
+
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+
+  event.preventDefault();
+  event.stopPropagation();
+}, "click-feedback");
