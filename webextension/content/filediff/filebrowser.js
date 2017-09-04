6cf2da296cc819ceb13341fe9ce31665f653fdff
Philipp Kewisch <mozilla@kewis.ch>
2017-09-04 21:47:50 +0200

    Add a feature to hide comments in file viewer

--- webextension/content/filediff/filebrowser.js
+++ webextension/content/filediff/filebrowser.js
@@ -46,6 +46,37 @@ createCommand("amoqueue-hide-delete", "Toggle deleted lines", "+", () => {
   document.body.classList.toggle("amoqueue-hide-delete");
 });
 
+var commentLazyInitialized = false;
+createCommand("amoqueue-hide-delete", "Toggle comment lines", "*", () => {
+  if (!commentLazyInitialized) {
+    for (let row of document.querySelectorAll(".tr-line")) {
+      let linecode = row.querySelector(".line-code");
+
+      // This is for file diffs, where the structure is slightly different
+      if (linecode.firstElementChild && linecode.firstElementChild.className == "plain") {
+        linecode = linecode.firstElementChild;
+      }
+
+      if (linecode.children.length == 2 && linecode.children[0].classList.contains("spaces") && linecode.children[1].classList.contains("comments")) {
+        // full line comments with some weird spaces up front
+        row.classList.add("amoqueue-comment-only");
+      } else if (linecode.children.length == 1 && linecode.children[0].classList.contains("comments")) {
+        // full line comments without spacing
+        row.classList.add("amoqueue-comment-only");
+      }
+    }
+
+    commentLazyInitialized = true;
+  }
+
+  let content = document.getElementById("content") || document.getElementById("diff");
+  let tablepos = content.querySelector(":scope > div > table").getBoundingClientRect();
+  let toprow = document.elementFromPoint(tablepos.x, Math.max(0, tablepos.y));
+
+  document.body.classList.toggle("amoqueue-hide-comments");
+  toprow.scrollIntoView(true);
+});
+
 createCommand("amoqueue-copy-slug", "Copy Slug", null, () => {
   let slug = document.querySelector("a.command[href*='editors/review']").getAttribute("href").split("/")[4];
 
