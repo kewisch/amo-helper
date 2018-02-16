94d8d28d596fcdd9283c12ed3e20f10f02c2b8a2
Philipp Kewisch <mozilla@kewis.ch>
2018-02-16 15:41:28 +0100

    Use subscription key directly

--- background/translation.js
+++ background/translation.js
@@ -3,47 +3,24 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * Portions Copyright (C) Philipp Kewisch, 2017 */
 
-const TOKEN_TTL = 1000 * 60 * 10;
-
-let gAuthToken;
-let gTokenTimeout = 0;
-
 browser.runtime.onMessage.addListener((data, sender) => {
   if (data.action !== "translate") {
     return undefined;
   }
 
-  return (async () => {
-    let key = await getStoragePreference("translation-secret-key");
-    if (!key) {
-      return { error: "API key not set" };
-    }
-
-    if (gTokenTimeout < Date.now()) {
-      let reqHeaders = new Headers({ "Ocp-Apim-Subscription-Key": key });
-      let url = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
-      let resp;
-      try {
-        resp = await fetch(url, { method: "POST", headers: reqHeaders });
-      } catch (e) {
-        return { error: `Request failure, service may be down (${e.message})` };
-      }
-
-      if (resp.ok) {
-        gAuthToken = await resp.text();
-        gTokenTimeout = Date.now() + TOKEN_TTL;
-      } else {
-        return { error: `Request failure, check API key (${resp.status} ${resp.statusText})` };
-      }
-    }
-    return translate(data.text);
-  })();
+  return translate(data.text);
 });
 
 async function translate(text) {
-  let reqHeaders = new Headers({ Authorization: "Bearer " + gAuthToken });
+  let key = await getStoragePreference("translation-secret-key");
+  if (!key) {
+    return { error: "API key not set" };
+  }
+
+  let reqHeaders = new Headers({ "Ocp-Apim-Subscription-Key": key });
   let url = `https://api.microsofttranslator.com/V2/Http.svc/Translate?text=${encodeURIComponent(text)}&to=en`;
   let resp;
+
   try {
     resp = await fetch(url, { method: "GET", headers: reqHeaders });
   } catch (e) {
@@ -57,10 +34,3 @@ async function translate(text) {
     return { error: `Request failure, check API key (${resp.status} ${resp.statusText})` };
   }
 }
-
-browser.storage.onChanged.addListener((changes, area) => {
-  if (area == "local" && changes["translation-secret-key"]) {
-    gTokenTimeout = 0;
-    gAuthToken = null;
-  }
-});
